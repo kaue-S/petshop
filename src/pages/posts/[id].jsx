ea390e0616547032c7b5151f65115240f1a37359
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-01-23 11:24:12 -0300

    condicao para getStaticPaths

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,3 +1,40 @@
-export default function Post() {
-  return <h2>Página post</h2>;
+import Container from "@/components/ui/Container";
+import styled from "styled-components";
+import Head from "next/head";
+
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  console.log(id);
 }
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+export default function Post() {
+  return (
+    <>
+      <Head>
+        <title>Titulo do post... - PetShop</title>
+        <meta name="description" content="Descrição do post..." />
+      </Head>
+
+      <StyledPost>
+        <h2>Titulo do post</h2>
+        <Container>
+          <h3>Categoria do post...</h3>
+          <p>Descrição do post</p>
+        </Container>
+      </StyledPost>
+    </>
+  );
+}
+
+const StyledPost = styled.article`
+  h2::before {
+    content: 📃;
+  }
+`;
