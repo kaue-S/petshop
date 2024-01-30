3ddb1981fc00a38b8960d448545a19e5bf583536
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-01-30 11:57:21 -0300

    comit

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -1,8 +1,34 @@
 import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import serverApi from "./api/server";
 
 export default function Contato() {
+  const { register, handleSubmit } = useForm();
+  let router = useRouter();
+
+  const enviarContato = async (dados) => {
+    const { nome, email, mensagem } = dados;
+
+    const opcoes = {
+      method: "POST",
+      body: JSON.stringify({ nome, email, mensagem }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+
+    try {
+      await fetch(`${serverApi}/contatos.json`, opcoes);
+      alert("Dados enviados");
+      router.push("/");
+    } catch (error) {
+      console.error("Deu ruim " + error.message);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -17,18 +43,30 @@ export default function Contato() {
         <h2>Fale Conosco</h2>
 
         <Container>
-          <form action="" method="post">
+          <form
+            action=""
+            method="post"
+            onSubmit={handleSubmit((dados) => {
+              enviarContato(dados);
+            })}
+          >
             <div>
               <label htmlFor="nome">Nome: </label>
-              <input type="text" name="nome" id="nome" />
+              <input {...register("nome")} type="text" name="nome" id="nome" />
             </div>
             <div>
               <label htmlFor="email">E-mail: </label>
-              <input type="email" name="email" id="email" />
+              <input
+                {...register("email")}
+                type="email"
+                name="email"
+                id="email"
+              />
             </div>
             <div>
               <label htmlFor="mensagem">Mensagem:</label>
               <textarea
+                {...register("mensagem")}
                 maxLength={500}
                 name="mensagem"
                 id="mensagem"
